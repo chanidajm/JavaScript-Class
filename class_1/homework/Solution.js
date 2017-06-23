4f59819b2b24e9952948e5d4782b7b96dcc47eab
Chanita <chanidajm@yahoo.com>
2017-06-23 10:43:55 -0500

    uploading homework

---
+++ class_1/homework/Solution.js
@@ -0,0 +1,4 @@
+var independenceday = new Date('July 4, 1776 23:15:30');
+var weekday = independenceday.getDate();
+
+console.log(weekday);
---
+++ class_2/Project1.js
@@ -0,0 +1,6 @@
+var number = prompt('Please type your number');
+var number2 = prompt('Please type your number');
+alert('All done, Ready for the result?');
+var number3 = parseInt(number) + parseInt(number2);
+alert(number3);
+document.write(number3);
